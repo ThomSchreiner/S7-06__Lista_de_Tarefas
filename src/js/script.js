5b3c46857f5a6842620dc825f70fe21046613d04
Thomas Schreiner <iteano.thom@hotmail.com>
2022-07-11 15:06:07 -0300

    criando lista de tarefas

---
+++ src/js/script.js
@@ -0,0 +1,59 @@
+let urgencia = {
+    urgente: "ponto-laranja",
+    prioritario: "ponto-amarelo",
+    normal: "ponto-verde"
+}
+
+function criarTarefa(valorTarefa) {
+
+    for(let i = 0; i < valorTarefa.length; i++) {
+
+        let liTarefa = criarCardTarefa(valorTarefa[i])
+        
+        let ul = ""
+        if(valorTarefa[i].tipo == "urgente") {
+            ul = document.querySelector(".urgente")
+            ul.appendChild(liTarefa)
+        } else if(valorTarefa[i].tipo == "prioritario") {
+            ul = document.querySelector(".prioritario")
+            ul.appendChild(liTarefa)
+        } else if(valorTarefa[i].tipo == "normal") {
+            ul = document.querySelector(".normal")
+            ul.appendChild(liTarefa)
+        }
+    }
+}
+
+criarTarefa(tarefas)
+
+function criarCardTarefa(tarefaAtual) {
+
+    let li = document.createElement("li")
+    let divNome = document.createElement("div")
+    let divPonto = document.createElement("div")
+    let pNome = document.createElement("p")
+    let button = document.createElement("button")
+    let img = document.createElement("img")
+
+    divNome.classList.add("tarefa-nome")
+    divPonto.classList.add(urgencia[tarefaAtual.tipo])
+    pNome.innerText = tarefaAtual.titulo
+    img.src = "src/img/Lixeira.png"
+    img.alt = "Icone Lixeira"
+    
+    divNome.appendChild(divPonto)
+    divNome.appendChild(pNome)
+    button.appendChild(img)
+    li.appendChild(divNome)
+    li.appendChild(button)
+    
+    return li
+}
+
+// let botaoLixeira = document.querySelector("li")
+// console.log(botaoLixeira)
+// botaoLixeira.addEventListener("click", function(event){
+//     botaoLixeira.setAttribute("hidden", "")
+//     console.log(botaoLixeira)
+    
+// })
---
+++ src/js/tarefas.js
@@ -0,0 +1,22 @@
+let tarefas = [
+    {
+      titulo: "Comprar comida para o gato",
+      tipo: "urgente"
+    },
+    {
+      titulo: "Limpar o quarto",
+      tipo: "normal"
+    },  
+    {
+      titulo: "Consertar Computador",
+      tipo: "prioritario"
+    },  
+    {
+      titulo: "Guardar dinheiro do lanche",
+      tipo: "urgente"
+    },  
+    {
+      titulo: "Beber água",
+      tipo: "prioritario"
+    }    
+]
